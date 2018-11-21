c79578b9596f2537ab8cffbd094e7b3ff7612fdf
pintukumar-ucreate <pintukumar@ucreate.co.in>
2018-11-21 17:59:53 +0530

    minor change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Chaos monkey
                 </div>
 
                 <div class="links">
